98f84c6894e10dad8bd8ec77db65162ab5243466
Sydney Faux <48231621+sfaux@users.noreply.github.com>
2020-04-04 18:52:34 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -7,9 +7,9 @@ from io import BytesIO
 
 from fastai.vision import *
 
-model_file_url = 'https://www.dropbox.com/s/y4kl2gv1akv7y4i/stage-2.pth?raw=1'
-model_file_name = 'model'
-classes = ['black', 'grizzly', 'teddys']
+model_file_url = 'https://drive.google.com/uc?export=download&id=163lU2vEmzS0N54pfWp2lYfypUXzGHfL5'
+model_file_name = 'waste-sorter'
+classes = ['paper','cardboard','glass','metal','plastic','trash']
 path = Path(__file__).parent
 
 app = Starlette()
